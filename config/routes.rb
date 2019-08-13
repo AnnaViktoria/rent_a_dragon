a42014300dc7ed321a1418356ca907d344a1affb
Nicolo Buzzi <nicolo@buzzi.org>
2019-08-13 14:36:40 +0200

    user deve

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'dragons#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root to: 'dragons#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+  resources :accounts, only: %i[index]
 end
