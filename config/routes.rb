e3eba9cfdbb3ead2590b2b0d5bfd0ca95a72edbf
Anna Gustafsson <annaviktoriagustafsson@gmail.com>
2019-08-13 15:29:57 +0200

    Style routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dragons#index'
   resources :dragons do
-  resources :reservations
-  resources :reviews, only: [:new, :create]
+    resources :reservations
+    resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:edit, :update, :destroy]
-  
 end
 
