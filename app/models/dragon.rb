d456c2471d034b14fc727dadb9e51896672b31eb
Anna Gustafsson <annaviktoriagustafsson@gmail.com>
2019-08-14 11:53:15 +0200

    created map with mapbox - added 'go back'

--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -1,8 +1,12 @@
 class Dragon < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :age, presence: true, format: { with: /\A\d+\z/, message: "Age should be a number." }
   validates :country_of_origin, presence: true
   validates :location, presence: true
+
 end
