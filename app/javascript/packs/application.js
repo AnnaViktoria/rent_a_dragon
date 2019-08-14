f41d871f2a36f4b20a07379f5821a03cd83d9465
Anna Gustafsson <annaviktoriagustafsson@gmail.com>
2019-08-14 14:57:24 +0200

    fixed the js issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
 import "bootstrap";
-import "../plugins/flatpickr";
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+import "../plugins/flatpickr";
+
 
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,9 +3,12 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/themes/material_green.css";
 
 const inputField = document.getElementById("reservation_start_date");
-const unavailableDates = inputField.dataset.disabledDates.split(";");
-flatpickr(".datepicker", {
-  disable: unavailableDates,
-  dateFormat: "d-m-Y",
-  minDate: "today"
-})
+if (inputField) {
+    const unavailableDates = inputField.dataset.disabledDates.split(";");
+    flatpickr(".datepicker", {
+    disable: unavailableDates,
+    dateFormat: "d-m-Y",
+    minDate: "today"
+  })
+}
+
